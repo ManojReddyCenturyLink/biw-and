b0ffbe8db6c929d57b98cc527f90794443627c59
DeAnthonee Greenfield <d.greenfield@accenture.com>
2020-03-30 10:14:34 -0500

    Correct accidental Removal of DI in base activity

--- app/src/main/java/com/centurylink/biwf/base/BaseActivity.kt
+++ app/src/main/java/com/centurylink/biwf/base/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.centurylink.biwf.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.android.AndroidInjection
 
 /**
  * Base class for holding common functionalities that will be used across screens.
@@ -10,5 +11,6 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidInjection.inject(this)
     }
 }
