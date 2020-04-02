bf9d99e1420f46ff856ebd4e07c7ea7f155d6f3c
pravin.kumar <pravin.kumar@accenture.com>
2020-04-02 11:24:22 +0530

    Fixed the Review Comments

--- app/src/main/java/com/centurylink/biwf/di/module/AppModule.kt
+++ app/src/main/java/com/centurylink/biwf/di/module/AppModule.kt
@@ -9,7 +9,6 @@ import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
 import javax.inject.Singleton
 
 @Module(includes = [ViewModelModule::class])
