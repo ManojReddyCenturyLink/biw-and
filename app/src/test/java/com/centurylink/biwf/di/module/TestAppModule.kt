6ddfef85e15a816e8de3162da6c1507e0f3c3b9b
pravin.kumar <pravin.kumar@accenture.com>
2020-04-08 19:27:31 +0530

    Fixed Review Comments

--- app/src/test/java/com/centurylink/biwf/di/module/TestAppModule.kt
+++ app/src/test/java/com/centurylink/biwf/di/module/TestAppModule.kt
@@ -1,12 +1,7 @@
 package com.centurylink.biwf.di.module
-import android.content.Context
-import com.centurylink.biwf.BIWFApp
 import com.centurylink.biwf.network.api.ApiServices
-import dagger.Module
-import io.mockk.mockk
 import org.amshove.kluent.mock
 
 class TestAppModule :AppModule(){
     override fun provideRetrofitService(): ApiServices = mock()
-
 }
