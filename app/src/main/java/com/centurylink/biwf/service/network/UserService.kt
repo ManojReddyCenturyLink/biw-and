ff05cb29614aa31840878208f8ce8b2f371333d1
pravin.kumar <pravin.kumar@accenture.com>
2020-05-06 01:21:02 +0530

    Fixed the Base Code for User Details

---
+++ app/src/main/java/com/centurylink/biwf/service/network/UserService.kt
@@ -0,0 +1,21 @@
+package com.centurylink.biwf.service.network
+
+import com.centurylink.biwf.model.user.UpdatedPassword
+import com.centurylink.biwf.model.user.UserDetails
+import com.centurylink.biwf.model.user.UserInfo
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface UserService {
+
+    @GET("sobjects/User")
+    suspend fun qetUserInfo(): UserInfo
+
+    @POST("sobjects/User/{user-id}/password")
+    suspend fun updatePassword(@Path("user-id") id: String, @Body updatedPassword: UpdatedPassword)
+
+    @GET("sobjects/User/{user-id}")
+    suspend fun getCompleteUserDetails(@Path("user-id") id: String): UserDetails
+}
