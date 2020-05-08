78d40f05e82889fc7980a5b1aabe78ba01c42db9
pravin.kumar <pravin.kumar@accenture.com>
2020-05-08 18:42:19 +0530

    Fixed the basic code

--- app/src/main/java/com/centurylink/biwf/screens/subscription/SubscriptionStatementActivity.kt
+++ app/src/main/java/com/centurylink/biwf/screens/subscription/SubscriptionStatementActivity.kt
@@ -1,6 +1,70 @@
 package com.centurylink.biwf.screens.subscription
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.centurylink.biwf.R
 import com.centurylink.biwf.base.BaseActivity
+import com.centurylink.biwf.coordinators.CancelSubscriptionCoordinator
+import com.centurylink.biwf.coordinators.Navigator
+import com.centurylink.biwf.coordinators.StatementCoordinator
+import com.centurylink.biwf.databinding.ActivitySubscriptionStatementBinding
+import com.centurylink.biwf.utility.DaggerViewModelFactory
+import javax.inject.Inject
+
 
 class SubscriptionStatementActivity : BaseActivity() {
+
+    companion object{
+        fun newIntent(context: Context) = Intent(context, SubscriptionStatementActivity::class.java)
+    }
+
+    @Inject
+    lateinit var statementCoordinator: StatementCoordinator
+
+    @Inject
+    lateinit var factory: DaggerViewModelFactory
+
+    @Inject
+    lateinit var navigator: Navigator
+
+    private val subscriptionStatementViewModel by lazy {
+        ViewModelProvider(this, factory).get(SubscriptionStatementViewModel::class.java)
+    }
+    private lateinit var binding: ActivitySubscriptionStatementBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySubscriptionStatementBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navigator.observe(this)
+        initHeaders()
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
+
+    private fun initHeaders(){
+        binding.activityHeaderView.apply{
+            subheaderCenterTitle.text = getText(R.string.statment_header)
+            subHeaderLeftIcon.setOnClickListener { finish() }
+            subheaderRightActionTitle.text = getText(R.string.statment_done)
+            subheaderRightActionTitle.setOnClickListener {
+
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            CancelSubscriptionDetailsActivity.REQUEST_TO_CANCEL_SUBSCRIPTION->{
+                if (resultCode == CancelSubscriptionDetailsActivity.REQUEST_TO_ACCOUNT) {
+                    setResult(CancelSubscriptionDetailsActivity.REQUEST_TO_ACCOUNT)
+                    finish()
+                }
+            }
+        }
+    }
 }
