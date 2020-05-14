b44b241096dcff883a05cba47877dff5eb462945
pravin.kumar <pravin.kumar@accenture.com>
2020-05-15 02:23:27 +0530

    Formatted Code

--- app/src/main/java/com/centurylink/biwf/service/network/ZuoraPaymentService.kt
+++ app/src/main/java/com/centurylink/biwf/service/network/ZuoraPaymentService.kt
@@ -10,7 +10,12 @@ import retrofit2.http.Query
 interface ZuoraPaymentService {
 
     @GET("query/")
-    suspend fun getZuoraPaymentDetails(@Query("q", encoded = true) id: String): FiberServiceResult<PaymentList>
+    suspend fun getZuoraPaymentDetails(
+        @Query(
+            "q",
+            encoded = true
+        ) id: String
+    ): FiberServiceResult<PaymentList>
 
     @GET("sobjects/Zuora__Payment__c//{invoice-id}")
     suspend fun getPaymentDetails(@Path("account-id") id: String): FiberServiceResult<AccountDetails>
