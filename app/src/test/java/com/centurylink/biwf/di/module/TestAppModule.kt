da0f0f01c3f40eed54c178cfb6ef8b5eb81c538b
pravin.kumar <pravin.kumar@accenture.com>
2020-05-21 23:08:13 +0530

    Reverted the code to Master

--- app/src/test/java/com/centurylink/biwf/di/module/TestAppModule.kt
+++ app/src/test/java/com/centurylink/biwf/di/module/TestAppModule.kt
@@ -1,31 +1,7 @@
 package com.centurylink.biwf.di.module
+import com.centurylink.biwf.service.network.ApiServices
+import org.amshove.kluent.mock
 
-import com.centurylink.biwf.service.impl.network.EitherCallAdapterFactory
-import com.centurylink.biwf.service.impl.network.EitherConverterFactory
-import com.centurylink.biwf.service.impl.network.FiberErrorConverterFactory
-import dagger.Provides
-import okhttp3.mockwebserver.MockWebServer
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
-
-class TestAppModule : AppModule() {
-
-    @Singleton
-    @Provides
-    fun provideMockServer(): MockWebServer {
-        return MockWebServer()
-    }
-
-    @Singleton
-    @Provides
-    fun provideRetrofitForMock(mockWebServer: MockWebServer): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(mockWebServer.url("/"))
-            .addCallAdapterFactory(EitherCallAdapterFactory())
-            .addConverterFactory(EitherConverterFactory())
-            .addConverterFactory(FiberErrorConverterFactory())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+class TestAppModule :AppModule(){
+    fun provideRetrofitService(): ApiServices = mock()
 }
