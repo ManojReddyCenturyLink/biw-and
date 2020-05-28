993773ab75d3a11178a6ac3fe5edad53d938fe3b
anton <anton@intrepid.io>
2020-05-28 17:20:23 -0400

    Upgrade to Android Studio 4.0

--- app/src/main/java/com/centurylink/biwf/BIWFApp.kt
+++ app/src/main/java/com/centurylink/biwf/BIWFApp.kt
@@ -1,9 +1,7 @@
 package com.centurylink.biwf
 
+import android.app.Application
 import android.content.Context
-import androidx.multidex.BuildConfig
-import androidx.multidex.MultiDex
-import androidx.multidex.MultiDexApplication
 import com.centurylink.biwf.coordinators.Navigator
 import com.centurylink.biwf.utility.InitUtility
 import com.jakewharton.threetenabp.AndroidThreeTen
@@ -13,7 +11,7 @@ import dagger.android.HasAndroidInjector
 import timber.log.Timber
 import javax.inject.Inject
 
-open class BIWFApp : MultiDexApplication(),HasAndroidInjector {
+open class BIWFApp : Application(),HasAndroidInjector {
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
@@ -35,7 +33,6 @@ open class BIWFApp : MultiDexApplication(),HasAndroidInjector {
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
-        MultiDex.install(this)
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
