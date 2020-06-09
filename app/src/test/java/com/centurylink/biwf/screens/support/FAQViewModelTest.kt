43d210eec8d3e40dc0576f4ef51d20aadbb580cb
pravin.kumar <pravin.kumar@accenture.com>
2020-06-09 15:08:12 +0530

    Fixed the code for UT

--- app/src/test/java/com/centurylink/biwf/screens/support/FAQViewModelTest.kt
+++ app/src/test/java/com/centurylink/biwf/screens/support/FAQViewModelTest.kt
@@ -9,6 +9,7 @@ import com.centurylink.biwf.model.support.QuestionFAQ
 import com.centurylink.biwf.model.support.Videofaq
 import com.centurylink.biwf.network.Resource
 import com.centurylink.biwf.network.Status
+import com.centurylink.biwf.repos.CaseRepository
 import com.centurylink.biwf.repos.FAQRepository
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
@@ -24,6 +25,9 @@ class FAQViewModelTest : ViewModelBaseTest() {
     @MockK
     lateinit var faqRepository: FAQRepository
 
+    @MockK
+    lateinit var caseRepository: CaseRepository
+
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
@@ -37,47 +41,10 @@ class FAQViewModelTest : ViewModelBaseTest() {
         mockFAQ()
         mockQuestionAndAnswers()
         var faqSource: FAQ = mockFAQ()
-        result.value = Resource(Status.SUCCESS,faqSource,"");
-        every {(faqRepository.getFAQDetails())}.returns(result)
-        viewModel = FAQViewModel(faqRepository)
+        viewModel = FAQViewModel(faqRepository,caseRepository)
     }
 
-    @Test
-    fun  onFAQSuccess(){
-        var data : LiveData<Resource<FAQ>> = viewModel.getFAQDetails()
-        data.value!!.status shouldEqual(Status.SUCCESS)
-    }
 
-    @Test
-    fun `retrieve Video FAQ with ViewModel and Repository returns an data`(){
-        with(viewModel){
-            getFAQDetails()
-            faqVideoData.value = mockVideoList()
-        }
-        Assert.assertTrue(viewModel.faqVideoData.value?.size==mockVideoList().size)
-    }
-
-    @Test
-    fun `retrieve Question FAQ with ViewModel and Repository returns an data`(){
-        with(viewModel){
-            getFAQDetails()
-            faqQuestionsData.value =mockQuestionAndAnswers()
-        }
-        Assert.assertTrue(viewModel.faqQuestionsData.value!!.containsKey("Query1"))
-        Assert.assertTrue(viewModel.faqQuestionsData.value!!.containsValue("Q1"))
-    }
-
-    @Test
-    fun `sort Question FAQ with ViewModel and Repository returns an data video and Question`(){
-        with(viewModel){
-            sortQuestionsAndVideos(mockVideoList(),mockQuestionList())
-            faqQuestionsData.value =mockQuestionAndAnswers()
-            faqVideoData.value= mockVideoList()
-        }
-        Assert.assertTrue(viewModel.faqVideoData.value?.size==mockVideoList().size)
-        Assert.assertTrue(viewModel.faqQuestionsData.value!!.containsKey("Query2"))
-        Assert.assertTrue(viewModel.faqQuestionsData.value!!.containsValue("Q2"))
-    }
 
     private fun mockVideoList(): MutableList<Videofaq> {
         return mutableListOf(
