b2fce37f59edbc24153e9f3938106c23a54808ba
pravin.kumar <pravin.kumar@accenture.com>
2020-06-09 18:40:25 +0530

    Removed the Unneccessary Repository

--- app/src/main/java/com/centurylink/biwf/di/module/RepositoryModule.kt
+++ app/src/main/java/com/centurylink/biwf/di/module/RepositoryModule.kt
@@ -1,17 +1,9 @@
 package com.centurylink.biwf.di.module
 
-import com.centurylink.biwf.repos.CurrentAppointmentRepository
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 object RepositoryModule {
 
-    @Provides
-    @Singleton
-    @JvmStatic
-    fun provideCurrentAppointmentRepo(): CurrentAppointmentRepository {
-        return CurrentAppointmentRepository()
-    }
+
 }
--- app/src/main/java/com/centurylink/biwf/repos/SupportRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.centurylink.biwf.repos
-
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class SupportRepository @Inject constructor() {
-
-}
