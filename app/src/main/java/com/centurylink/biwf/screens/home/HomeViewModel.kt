a6893bc55d68e790bcf236f007677f1b7607ac91
pravin.kumar <pravin.kumar@accenture.com>
2020-07-27 14:55:07 +0530

    Fixed the address Path

--- app/src/main/java/com/centurylink/biwf/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/centurylink/biwf/screens/home/HomeViewModel.kt
@@ -128,6 +128,7 @@ class HomeViewModel @Inject constructor(
         accountDetails.fold(ifLeft = {
             errorMessageFlow.latestValue = it
         }) {
+            it.accountStatus ="dsd"
             if (it.accountStatus.equals("Pending Activation", true)) {
                 activeUserTabBarVisibility.latestValue = false
                 progressViewFlow.latestValue = false
