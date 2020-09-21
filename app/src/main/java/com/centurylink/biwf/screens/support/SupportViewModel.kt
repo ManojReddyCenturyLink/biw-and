beb5d54c9c34157d5ca815484514846467abd927
shruti.pahuja <shruti.pahuja@accenture.com>
2020-09-21 16:47:01 +0530

    Added speed test flag update

--- app/src/main/java/com/centurylink/biwf/screens/support/SupportViewModel.kt
+++ app/src/main/java/com/centurylink/biwf/screens/support/SupportViewModel.kt
@@ -101,9 +101,10 @@ class SupportViewModel @Inject constructor(
     private fun getSpeedTestId() {
         progressVisibility.latestValue = true
         speedTestButtonState.latestValue = false
+        sharedPreferences.saveSpeedTestFlag(boolean = true)
         modemResetButtonState.latestValue = false
+        latestSpeedTest.latestValue = DashboardViewModel.EMPTY_RESPONSE
         viewModelScope.launch {
-            latestSpeedTest.latestValue = DashboardViewModel.EMPTY_RESPONSE
             val speedTestRequest = assiaRepository.startSpeedTest()
             speedTestRequest.fold(
                 ifRight = {
