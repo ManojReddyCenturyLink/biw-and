df1cb9b0c593b8621b6ec9b90f8f70ed8ec5f70e
pravin.kumar <pravin.kumar@accenture.com>
2020-10-06 17:35:38 +0530

    Fixed the Code comment for McAfee Repository

--- app/src/main/java/com/centurylink/biwf/repos/McafeeRepository.kt
+++ app/src/main/java/com/centurylink/biwf/repos/McafeeRepository.kt
@@ -14,11 +14,27 @@ import com.centurylink.biwf.utility.preferences.Preferences
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * McafeeRepository - This class interacts with McafeeApiService Services. This Repository class
+ * gets the data from the network . It handles all the Devices related  information from the McAfee Apis
+ * backend  and the View models can consume the McAfee Devices related information and display in the Activity
+ * or Fragments.
+ *
+ * @property preferences preferences preference Instance for storing the value in shared preferences.
+ * @property mcaFeeService Instance for interacting with the McAfee API.
+ * @constructor Create empty Mcafee repository
+ */
 @Singleton
 class McafeeRepository @Inject constructor(
     private val preferences: Preferences,
     private val mcaFeeService: McafeeApiService
 ) {
+    /**
+     * The suspend function is used to get the list of Devices connected to the Modem.
+     *
+     * @param deviceMacAddresses The List of MAcAddress will be passed from Cloudcheck API
+     * @return list of MacDevicesList in case of Success and Error Message in case of Errors.
+     */
     suspend fun getMcafeeDeviceIds(deviceMacAddresses: List<String>): Either<String, List<MacDeviceList>> {
         val result =
             mcaFeeService.getDevicesMapping(MappingRequest(preferences.getAssiaId(), deviceMacAddresses))
@@ -30,6 +46,12 @@ class McafeeRepository @Inject constructor(
         }
     }
 
+    /**
+     * The suspend function returns the Status of the device in case of Pause / Failure
+     *
+     * @param deviceId The DeviceID of the devices
+     * @return DevicePauseStatus in case of Success and error message in case of Errors
+     */
     suspend fun getDevicePauseResumeStatus(deviceId: String): Either<String, DevicePauseStatus> {
         val result =
             mcaFeeService.getNetworkInfo(BlockRequest(preferences.getAssiaId(), deviceId))
@@ -41,6 +63,13 @@ class McafeeRepository @Inject constructor(
         }
     }
 
+    /**
+     * Updates the  device pause/ resume status to the backend.
+     *
+     * @param deviceId the DeviceId
+     * @param isPaused the status to be updated for the device.
+     * @return DevicePauseStatus in case of Success and Error message in case of errors.
+     */
     suspend fun updateDevicePauseResumeStatus(deviceId: String, isPaused: Boolean):
             Either<String, DevicePauseStatus> {
         val result = mcaFeeService.updateNetWorkInfo(
@@ -54,6 +83,14 @@ class McafeeRepository @Inject constructor(
         }
     }
 
+    /**
+     * Suspend function used to Update the device details
+     *
+     * @param deviceType The Devicetype info to be updated.
+     * @param deviceName The DeviceName info to be updated.
+     * @param deviceId The DeviceId to be Updated.
+     * @return Success message in case of Success and Error message in case of Error.
+     */
     suspend fun updateDeviceName(deviceType: String, deviceName: String, deviceId: String):
             Either<String, String> {
         val result = mcaFeeService.updateDeviceInfo(
