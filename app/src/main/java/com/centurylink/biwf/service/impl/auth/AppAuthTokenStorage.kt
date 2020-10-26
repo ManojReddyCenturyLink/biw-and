6094b8315514d5b5756792469c2663c6c592a09b
radha.cheerla <Hekshu@dec25>
2020-10-26 23:33:16 +0530

    encryption of auth token

--- app/src/main/java/com/centurylink/biwf/service/impl/auth/AppAuthTokenStorage.kt
+++ app/src/main/java/com/centurylink/biwf/service/impl/auth/AppAuthTokenStorage.kt
@@ -1,8 +1,7 @@
 package com.centurylink.biwf.service.impl.auth
 
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.security.crypto.EncryptedSharedPreferences
+import com.centurylink.biwf.service.auth.EncryptionServices
 import com.centurylink.biwf.service.auth.TokenStorage
 import com.centurylink.biwf.utility.BehaviorStateFlow
 import kotlinx.coroutines.flow.Flow
@@ -12,34 +11,27 @@ import javax.inject.Inject
 class AppAuthTokenStorage @Inject constructor(
     private val appContext: Context
 ) : TokenStorage<AuthState> {
-
-    private val preferences: SharedPreferences by lazy {
-        EncryptedSharedPreferences.create(
-            "AccountPrefData",
-            "${appContext.packageName}._preferences_data_",
-            appContext,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-    }
+    private var encryptedAuthStorage: String = ""
 
     private val authTokenKey = "_preferences_data_"
 
     override var state: AuthState?
         get() = synchronized(this) {
-            preferences.getString(authTokenKey, null)?.let { AuthState.jsonDeserialize(it) }
+            EncryptionServices(appContext).decrypt(encryptedAuthStorage, authTokenKey)
+                ?.let { AuthState.jsonDeserialize(it) }
         }
         set(value) = synchronized(this) {
+            val encryptionService = EncryptionServices(appContext)
+            encryptionService.createMasterKey(authTokenKey)
             val hasToken = if (value != null) {
-                preferences.edit()
-                    .putString(authTokenKey, value.jsonSerializeString())
-                    .apply()
+                encryptedAuthStorage =
+                    EncryptionServices(appContext).encrypt(
+                        value.jsonSerializeString(),
+                        authTokenKey
+                    )
                 true
 
             } else {
-                preferences.edit()
-                    .remove(authTokenKey)
-                    .apply()
                 false
             }
 
